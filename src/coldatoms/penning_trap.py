f3861d87c263f761a4fc58f7b3db86b7f6a993a8
Wes-Lee-Johnson <quantumcat.wj@gmail.com>
2023-07-11 14:32:13 -0600

    added rotating wall trap potential

--- src/coldatoms/penning_trap.py
+++ src/coldatoms/penning_trap.py
@@ -42,3 +42,83 @@ class HarmonicTrapPotential(object):
                 positions, q, self.kx, self.ky, self.kz, self.phi, dt, f)
         else:
             raise RuntimeError('Must provide a charge to compute coulomb force')
+
+class RotatingWallTrapPotential(object):
+    """
+    This class defines the trap potential. It is used to calculate the
+    forces on the ions due to the trap potential. This includes the
+    rotating wall potential. 
+
+    Parameters
+    ----------
+    kz : float
+        The axial stiffness.
+    delta : float
+        The rotating wall strength.
+    omega : float
+        The rotating wall frequency.
+    phi_0 : float
+        The initial phase of the rotating wall.
+
+    Methods
+    -------
+    reset_phase()
+        Resets the phase of the rotating wall.
+    force(dt, ensemble, f)
+        Calculates the force on the ions due to the trap potential.
+
+    Examples
+    --------
+    >>> trap_potential = TrapPotential(m_Be/q_Be*wz**2, ma.delta, ma.wrot, np.pi / 2.0)
+    """
+    def __init__(self, kz, delta, omega, phi_0):
+        self.kz = kz
+        self.kx = -(0.5 + delta) * kz
+        self.ky = -(0.5 - delta) * kz
+        self.phi_0 = phi_0
+        self.phi = phi_0
+        self.omega = omega
+
+    def reset_phase(self):
+        """
+        Resets the phase of the rotating wall.
+        """
+        self.phi = self.phi_0
+
+    def force(self, dt, ensemble, f):
+        """
+        Calculates the force on the ions due to the trap potential.
+
+        Parameters
+        ----------
+        dt : float
+            The time step.
+        ensemble : Ensemble 
+            The ensemble of ions.
+        f : ndarray
+            The force on the ions.
+        """
+        self.phi += self.omega * 0.5 * dt
+
+        q = ensemble.ensemble_properties['charge']
+        if q is None:
+            q = ensemble.particle_properties['charge']
+            if q is None:
+                raise RuntimeError('Must provide ensemble or per particle charge')
+
+        cphi = np.cos(self.phi)
+        sphi = np.sin(self.phi)
+        kx = self.kx
+        ky = self.ky
+
+        x = ensemble.x[:, 0]
+        y = ensemble.x[:, 1]
+        z = ensemble.x[:, 2]
+
+        f[:, 0] += dt * q * (
+            (-kx * cphi * cphi - ky * sphi * sphi) * x + cphi * sphi * (ky - kx) * y)
+        f[:, 1] += dt * q * (
+            cphi * sphi * (ky - kx) * x + (-kx * sphi * sphi - ky * cphi * cphi) * y)
+        f[:, 2] += -dt * q *self.kz * z
+
+        self.phi += self.omega * 0.5 * dt
